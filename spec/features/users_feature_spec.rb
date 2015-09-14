9fe334259fce35a7eb0922f04fa938fa4dc3d011
Ben <ben.c.hawker@gmail.com>
2015-09-14 14:46:09 +0100

    Rails setup and user attributes done

---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+feature 'User can sign in & out' do
+	context 'User not signed in & on the homepage' do
+		it 'should see a "sign-in" link & "sign-out" link' do
+			visit '/'
+			expect(page).to have_link('Sign in')
+			expect(page).to have_link('Sign up')
+		end
+
+		it 'should not see sign out link' do
+			visit '/'
+			expect(page).not_to have_link('Sign out')
+		end
+	end
+
+	context 'User signed in on the homepage' do
+		before do
+			visit '/'
+			click_link('Sign up')
+			fill_in 'Email', with: 'test@test.com'
+			fill_in 'Password', with: 'testtest'
+			fill_in 'Password confirmation', with: 'testtest'
+			click_button('Sign up')
+		end
+
+		it 'should see link for "Sign out"' do
+			visit '/'
+			save_and_open_page
+			expect(page).to have_link('Sign out')
+		end
+
+		it 'should not see a "sign in" link nor "Sign up" link' do
+			visit '/'
+			expect(page).not_to have_link('Sign in')
+			expect(page).not_to have_link('Sign up')
+		end
+	end
+end
